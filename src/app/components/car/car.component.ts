685b306163d6787439b14f8f58521674840862c8
Emrah611 <bey_emrah@mail.ru>
2022-02-22 15:14:34 +0300

    front

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Car } from 'src/app/models/Car/car';
-import { CarResponseModel } from 'src/app/models/Car/carResponseModel';
 import { CarService } from 'src/app/services/car.service';
 
 @Component({
--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -2,19 +2,20 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
-import { Car } from '../models/Car/car';
+import { Brand } from '../models/Brand/brand';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CarService {
+export class BrandService {
 
+  apiUrl = "https://localhost:44316/api/brands/getall"
+  
   constructor(private httpClient:HttpClient) { }
 
-  apiUrl = "https://localhost:44316/api/cars/getall"
 
 
-  getCars():Observable<ListResponseModel<Car>>{
-    return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl);
+  getBrands():Observable<ListResponseModel<Brand>>{
+    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
   }
 }
